315524de178fa1dc1b0b7d2a309354c66c1b91ce
Rubical <zhpiv96@mail.ru>
2023-03-09 21:55:04 +0300

    upd

--- src/components/UI/Header/Header.js
+++ src/components/UI/Header/Header.js
@@ -3,32 +3,59 @@ import cl from "./Header.module.css";
 const Header = () => {
   return (
     <header className={cl.row}>
+      {/*Main */}
+
       <div className={cl.main}>
         <div className={cl.title}>Зарабатывайте больше</div>
         <h1 className={cl.titleLogo}>с WELBEX</h1>
         <p className={cl.titleText}>Развиваем и контролируем продажи за вас</p>
       </div>
+
+      {/*Advertising */}
+
       <div className={cl.advertising}>
-        <h2>Вместе с бесплатной консультацией мы дарим:</h2>
-        <div>
-          <div>
-            <h3></h3>
-            <p></p>
+        <div className={cl.advertsingTitle}>
+          <span className={cl.advertisingTitleDefault}> Вместе с </span>
+          <span className={cl.advertsingTitleStyled}>
+            БЕСПЛАТНОЙ КОНСУЛЬТАЦИЕЙ
+          </span>
+          <span className={cl.advertisingTitleDefault}> мы дарим</span>
+        </div>
+
+        <div className={cl.advertisingBlock}>
+          <div className={cl.advertisingBlockCard}>
+            <h3 className={cl.advertisingBlockCardTitle}>ВИДЖЕТЫ</h3>
+            <p className={cl.advertisingBlockCardText}>
+              30 готовых <br /> решений
+            </p>
           </div>
-          <div>
-            <h3></h3>
-            <p></p>
+          <div className={cl.advertisingBlockCard}>
+            <h3 className={cl.advertisingBlockCardTitle}>DASHBOARD</h3>
+            <p
+              style={{ maxWidth: "135px" }}
+              className={cl.advertisingBlockCardText}
+            >
+              с показателями вашего бизнеса
+            </p>
           </div>
-          <div>
-            <h3></h3>
-            <p></p>
+          <div className={cl.advertisingBlockCard}>
+            <h3 className={cl.advertisingBlockCardTitle}>SKYPE АУДИТ</h3>
+            <p className={cl.advertisingBlockCardText}>
+              отдела продаж
+              <br /> и CRM системы
+            </p>
           </div>
-          <div>
-            <h3></h3>
-            <p></p>
+          <div className={cl.advertisingBlockCard}>
+            <h3 className={cl.advertisingBlockCardTitle}>35 ДНЕЙ</h3>
+            <p className={cl.advertisingBlockCardText}>
+              использования <br /> CRM
+            </p>
           </div>
         </div>
-        <button>Получить консультацию</button>
+
+        {/*Button*/}
+
+        <button className={cl.button}>Получить консультацию</button>
       </div>
     </header>
   );
